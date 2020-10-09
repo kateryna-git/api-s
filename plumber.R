49e9a972364259644b1f49f9e033e545b96121b1
Kateryna <45816115+kateryna-git@users.noreply.github.com>
2020-10-09 14:09:45 +0300

    Update plumber.R

--- plumber.R
+++ plumber.R
@@ -3,21 +3,21 @@
 model <- readRDS("cars-model.rds")
 
 #* Plot a histogram of the gross horsepower
-#* @serializer png
+#* png
 #* @get /plothp
 function(){
   hist(mtcars$hp)
 }
 
 #* Plot a histogram of the manual transmission
-#* @serializer png
+#* png
 #* @get /plotam
 function(){
   hist(mtcars$am)
 }
 
 #* Plot a histogram of the weight (1000 lbs)
-#* @serializer png
+#* png
 #* @get /plotwt
 function(){
   hist(mtcars$wt)
