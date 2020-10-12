431c3b44dff21452cc6bb2ee43e58090e01c1c9e
Kateryna <45816115+kateryna-git@users.noreply.github.com>
2020-10-12 14:03:59 +0300

    Update plumber.R

--- plumber.R
+++ plumber.R
@@ -1,25 +1,24 @@
-library(modeltime)
-library(timetk)   
-library(lubridate)
-library(tidyverse)
-library(jsonlite)
-
-
-
-model <- readRDS("sales_data_sample.csv")
+model <- readRDS("cars-model.rds")
 
 #* Plot a histogram of the gross horsepower
 #* @serializer png
 #* png
-#* @get /plot
+#* @get /plothp
 function(){
-  plot <- raw_data %>%
-    select(SALES, ORDERDATE) %>% 
-    mutate(date = mdy_hm(ORDERDATE) %>% as_datetime()) %>%
-    group_by(date) %>%
-    summarise(value = sum(SALES)) %>%
-    ungroup() %>%
-    plot_time_series(date, value, .interactive = FALSE)
-  
-  return(plot)
+  hist(mtcars$hp)
 }
+
+#* Plot a histogram of the manual transmission
+#* @serializer png
+#* png
+#* @get /plotam
+function(){
+  hist(mtcars$am)
+}
+
+#* Plot a histogram of the weight (1000 lbs)
+#* @serializer png
+#* png
+#* @get /plotwt
+function(){
+  hist(mtcars$wt)
