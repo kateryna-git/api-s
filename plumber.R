5422e2e7aa454da91dae09eb2df7d31c46e04125
Kateryna <45816115+kateryna-git@users.noreply.github.com>
2020-10-12 13:06:25 +0300

    Update plumber.R

--- plumber.R
+++ plumber.R
@@ -1,33 +1,53 @@
 # plumber.R
 
-model <- readRDS("cars-model.rds")
 
-#* Plot a histogram of the gross horsepower
-#* @png
-#* @get /plothp
+#* @apiTitle Plumber Sales Analysis API
+library(modeltime)
+library(timetk)   
+library(lubridate)
+library(tidyverse)
+
+
+
+# model <- read_rds(here("10_shinydashboard/model-prophet-boost.rds"))
+raw_data <- read_csv('sales_data_sample.csv') #read only the needed fields to make faster
+
+
+#* Plot a time series plot of the data
+#* @serializer htmlwidget
+#* @get /plot
 function(){
-  hist(mtcars$hp)
+  plot <- raw_data %>%
+    select(SALES, ORDERDATE) %>% 
+    mutate(date = mdy_hm(ORDERDATE) %>% as_datetime()) %>%
+    group_by(date) %>%
+    summarise(value = sum(SALES)) %>%
+    ungroup() %>%
+    plot_time_series(date, value, .interactive = TRUE)
+  
+  return(plot)
 }
 
-#* Plot a histogram of the manual transmission
-#* @png
-#* @get /plotam
-function(){
-  hist(mtcars$am)
-}
+#* Returns filtered data
+#* @param forecast_period Forcast period in months
+#* @param time_unit Unit to aggregate by (month, day, week, year)
+#* @param left date range lower margin
+#* @param right date range upper margin 
+#* @post /raw_data
 
-#* Plot a histogram of the weight (1000 lbs)
-#* @png
-#* @get /plotwt
-function(){
-  hist(mtcars$wt)
-}
 
-#* Returns the probability whether the car has a manual transmission
-#* @param hp Gross horsepower
-#* @param wt Weight (1000 lbs)
-#* @post /manualtransmission
-function(hp, wt){
-  newdata <- data.frame(hp = as.numeric(hp), wt = as.numeric(wt))
-  predict(model, newdata, type = "response")
+preprocess_data <-function(forecast_period = "6 months", time_unit = "day", left = "2003-01-06", right = "2005-05-31") {
+  
+  df <- raw_data %>%    
+    select(SALES, ORDERDATE) %>% 
+    mutate(date = mdy_hm(ORDERDATE) %>% as_datetime()) %>% 
+    filter(date %>% between(as_datetime(left),
+                            as_datetime(right))) %>% 
+    mutate(date = floor_date(date,  # Round dates to beginning of a period
+                             unit = time_unit)) %>% 
+    group_by(date) %>%
+    summarise(value = sum(SALES)) %>%
+    ungroup()
+  
+  return(df)
 }
